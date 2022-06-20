54e3e532344d1cb9ef79f5cf035d098183aa205b
Kenichi Kamiya <kachick1@gmail.com>
2022-06-21 04:09:28 +0900

    [WIP] init [ci skip]

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,19 @@
 import { getInput, startGroup, endGroup } from '@actions/core';
 import { context } from '@actions/github';
 import { submitSnapshot } from '@github/dependency-submission-toolkit';
-
-import path from 'path';
+import { normalize } from 'path';
 
 import { buildSnapshot } from './elm-package-detector';
 
 async function run(): Promise<void> {
   startGroup('Setup variables');
   const snapshot = buildSnapshot(
-    path.normalize(getInput('elm-json-path', { required: true, trimWhitespace: true })),
+    normalize(getInput('elm-json-path', { required: true, trimWhitespace: true })),
     context.job,
     context.runId.toString(),
   );
+
+  // check dry_Run
 
   await submitSnapshot(snapshot);
   endGroup();
