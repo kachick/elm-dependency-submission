bdd1a67cddf9022165f1cd8716b5b801b7055a95
Kenichi Kamiya <kachick1@gmail.com>
2022-06-29 11:25:59 +0900

    Implement validations with defining zod schemas

--- src/elm-package-detector.ts
+++ src/elm-package-detector.ts
@@ -3,23 +3,34 @@ import { BuildTarget, Package, PackageCache, Snapshot } from '@github/dependency
 import { context } from '@actions/github';
 import { PackageURL } from 'packageurl-js';
 import { assertIsDefined } from './typeguards';
+import { z } from 'zod';
 
-type ElmDependencies = {
-  direct: { [key: string]: string };
-  indirect: { [key: string]: string };
-};
+// https://github.com/colinhacks/zod/tree/20a45a20a344c48fc8cd1b630adcb822d439e70d#json-type
+const literalSchema = z.union([z.string(), z.number(), z.boolean(), z.null()]);
+type Literal = z.infer<typeof literalSchema>;
+type Json = Literal | { [key: string]: Json } | Json[];
+const jsonSchema: z.ZodType<Json> = z.lazy(() => z.union([literalSchema, z.array(jsonSchema), z.record(jsonSchema)]));
+
+const elmDependenciesSchema = z.record(z.string(), z.string());
+
+type ElmDependencies = z.infer<typeof elmDependenciesSchema>;
+
+const elmDependenciesSetSchema = z.object({
+  direct: elmDependenciesSchema,
+  indirect: elmDependenciesSchema,
+});
 
 // https://github.com/elm/compiler/blob/9f1bbb558095d81edba5796099fee9981eac255a/docs/elm.json/package.md
-type ElmJSON = {
-  'type': 'application' | 'package';
-  name?: string;
-  license?: string;
-  version?: string;
-  'exposed-modules'?: string[];
-  'elm-version': 'string';
-  dependencies: ElmDependencies;
-  'test-dependencies': ElmDependencies;
-};
+const elmJSONSchema = z.object({
+  type: z.enum(['application', 'package']),
+  name: z.optional(z.string()),
+  license: z.optional(z.string()),
+  version: z.optional(z.string()),
+  'exposed-modules': z.optional(z.array(z.string())),
+  'elm-version': z.string(),
+  dependencies: elmDependenciesSetSchema,
+  'test-dependencies': elmDependenciesSetSchema,
+});
 
 export function parseNameAndNamespace(depPath: string): [string, string] {
   const namespaceAndName = depPath.split('/');
@@ -48,7 +59,7 @@ export function parseNameAndNamespace(depPath: string): [string, string] {
   }
 }
 
-export function parseDependencies(cache: PackageCache, dependencies: { [key: string]: string }): Array<Package> {
+export function parseDependencies(cache: PackageCache, dependencies: ElmDependencies): Array<Package> {
   return Object.entries(dependencies).map(([depPath, version]) => {
     const [namespace, name] = parseNameAndNamespace(depPath);
     const packageUrl = new PackageURL('elm', namespace, name, version, null, null);
@@ -56,7 +67,15 @@ export function parseDependencies(cache: PackageCache, dependencies: { [key: str
   });
 }
 
-export function createBuildTarget(elmJSON: ElmJSON): BuildTarget {
+export function createBuildTarget(elmJSONString: string): BuildTarget {
+  const maybeElmJSON = elmJSONSchema.safeParse(jsonSchema.parse(JSON.parse(elmJSONString)));
+
+  if (!maybeElmJSON.success) {
+    throw new Error(`given file is an invalid format of elm.json: ${maybeElmJSON.error.message}`);
+  }
+
+  const elmJSON = maybeElmJSON.data;
+
   const cache = new PackageCache();
   const topLevelDirectDependencies = parseDependencies(cache, elmJSON.dependencies.direct);
   const topLevelIndirectDependencies = parseDependencies(cache, elmJSON.dependencies.indirect);
@@ -80,8 +99,7 @@ export function createBuildTarget(elmJSON: ElmJSON): BuildTarget {
 }
 
 export function buildSnapshot(elmJsonPath: string, job: string, runId: string): Snapshot {
-  const elmJson = JSON.parse(readFileSync(elmJsonPath).toString()) as ElmJSON;
-  const buildTarget = createBuildTarget(elmJson);
+  const buildTarget = createBuildTarget(readFileSync(elmJsonPath).toString());
   const snapshot = new Snapshot(
     {
       name: 'kachick/elm-dependency-submission',
