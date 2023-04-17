c45795ad9779581a74689a2bf0ac358c8f1d1d98
Kenichi Kamiya <kachick1@gmail.com>
2023-04-18 02:54:34 +0900

    Bump nixpkgs to latest

--- default.nix
+++ default.nix
@@ -1,11 +1,11 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/b77bbeca8ba3069eea82a3711242cdc2240cc7c9.tar.gz") {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/4c3edba85629ec304b5269e4b0ac7f26c433df23.tar.gz") { } }:
 
 pkgs.mkShell {
   buildInputs = [
     pkgs.nodejs-16_x
     pkgs.dprint
+    pkgs.cargo-make
     pkgs.nil
     pkgs.nixpkgs-fmt
-    pkgs.cargo-make
   ];
 }
