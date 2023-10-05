42912b8814ed593ccefa08df2ece09c3067f6f97
Kenichi Kamiya <kachick1@gmail.com>
2023-10-05 20:19:31 +0900

    Prefer Nix flake and bump nixpkgs

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    # Candidate channels
+    #   - https://github.com/kachick/anylang-template/issues/17
+    #   - https://discourse.nixos.org/t/differences-between-nix-channels/13998
+    # How to update the revision
+    #   - `nix flake update --commit-lock-file` # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake-update.html
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # https://discourse.nixos.org/t/mark-a-devshell-dependency-as-insecure/24354/3
+        pkgs = import nixpkgs
+          {
+            inherit system;
+            config = {
+              permittedInsecurePackages = [
+                "nodejs-16.20.2"
+              ];
+            };
+          };
+      in
+      {
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs = [
+              # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
+              # https://github.com/kachick/dotfiles/pull/228
+              bashInteractive
+
+              nodejs_16
+              deno
+              dprint
+              cargo-make
+              nil
+              nixpkgs-fmt
+              typos
+              actionlint
+            ];
+          };
+      });
+}
